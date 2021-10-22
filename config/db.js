743f702cd312507091e2fbfd4ab01da14f29b50b
romibernard <romibernard68@gmail.com>
2021-10-22 09:26:07 -0500

    fix mongodb

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/artempo-server", {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
